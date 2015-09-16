a2002417c3444d5edec8a924ae89ad79562c08a1
derrickliu <liulinpeng@gmail.com>
2015-09-16 20:33:04 +0800

    小修改

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var arguments = process.argv.splice(2);
 
 var inputPath = arguments[0];
 
-var outputPath = arguments[1];
+var outputPath = arguments[1] || arguments[0];
 
 if(!fs.existsSync(outputPath)){
 	fs.mkdirSync(outputPath);
@@ -17,14 +17,23 @@ if(!fs.existsSync(outputPath)){
 
 var dirFiles = fs.readdirSync(inputPath);
 
-console.log(dirFiles.length + '个文件');
+var len = dirFiles.length;
 
-dirFiles.forEach(function(file){
-	console.log(inputPath + '/' + file);
-	gm(inputPath + '/' + file)
-		.resize()
-		.write(outputPath + '/' + file,function(err){
-			if(err) throw err;
-		});
+var c = function(i){
+	gm(inputPath + '/' + dirFiles[i])
 	
-});
+	.write(outputPath + '/' + dirFiles[i],function(err){
+		if(err) throw err;
+		else{
+			if(i < len - 1){
+				console.log(dirFiles[i] + ' 已完成压缩');
+				c(i+1);
+			}else{
+				console.log('\n\n大婶，压缩完成了！');
+			}
+		}
+	})
+};
+
+c(0);
+
